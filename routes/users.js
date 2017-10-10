4a17c736867076bfde4f9958847a0321c3443a0f
niyatpatel23295 <niyatpatel23295@gmail.com>
2017-10-10 02:29:40 -0700

    Add singIn API

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 var connection = require('./../utils/connection_helper.js');
 const crypto = require('crypto');
-/* GET users listing. */
+
+
+/* API for signup users with email and password*/
 router.post('/signUp', function(req, res, next) {
 	var salt = '';
 	crypto.randomBytes(256, function(err, buffer){
@@ -34,5 +36,40 @@ router.post('/signUp', function(req, res, next) {
 		}
 	});
 });
+
+// API for SignIn users
+router.post('/signIn', function(req, res, next) {
+
+	var getUserDataSQL = "SELECT email, password, salt FROM dropbox.useraccount where email like '" +req.body.email + "';";
+
+	// Execute SQL
+  	connection.executequery(getUserDataSQL, function(err, data){
+  		if(err){
+  			console.trace(err);
+  			res.status(500).json({"error": "Internal server error"});
+  		}
+  		else if( !(data.length > 0) ){
+  			res.status(200).json({"error": "User not found"});
+  		}
+  		else{
+  			var password_hash = data[0].password;
+  			var salt = data[0].salt;
+
+  			// compute password hash of given password
+  			var _hash = crypto.createHmac('sha512', salt);
+  			_hash.update(req.body.password);
+  			var _password_hash = _hash.digest('hex');
+
+  			// Check input password with stored password
+  			if(_password_hash !== password_hash){
+  				res.status(401).json({"error": "Incorrect Password"});
+  			}
+  			else{
+  				res.status(200).json({"message": "success"});
+  			}
+  		}
+  	});
+});
+
 
 module.exports = router;
