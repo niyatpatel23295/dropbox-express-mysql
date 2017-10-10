b0bd3caf417ba29b659886cb7830aad988a7932c
niyatpatel23295 <niyatpatel23295@gmail.com>
2017-10-10 03:48:34 -0700

    Create upload prototype at /file/upload API

--- routes/userfiles.js
+++ routes/userfiles.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('./../utils/connection_helper.js')
-/* GET users listing. */
-router.post('/upload', function(req, res, next) {
+var mysql = require('./../utils/connection_helper.js');
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+var fs = require('fs');
+var connection = require('./../utils/connection_helper.js');
 
+/* GET users listing. */
+router.post('/upload', upload.single('file'),  function(req, res, next) {
+	// Check is user is authentic
+	var getUserDataSQL = "SELECT uaid FROM useraccount WHERE email = ' +req.body.email + '";
+
+
+	fs.writeFile('./../files/' + req.body. + req.file.originalname, req.file.buffer, function(err, response){
+		if(err) console.log(err);
+		else res.send(response);
+	});
 });
 
 module.exports = router;
