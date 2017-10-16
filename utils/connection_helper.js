21fb61bec16f75593614328fdd5b0afd1ab7c1f7
Niyat Patel <niyatpatel23295@gmail.com>
2017-10-15 23:00:23 -0700

    Add support for connection pooling

--- utils/connection_helper.js
+++ utils/connection_helper.js
@@ -1,25 +1,57 @@
 var mysql = require('mysql')
 
-function getConnection(){
-	var connection = mysql.createConnection({
-	  	host     : process.env.DROPBOX_HOST,
-	  	user     : process.env.DROPBOX_USER,
-	  	password : process.env.DROPBOX_PASSWORD,
-	  	database : 'dropbox'
-	});	
-	return connection;
-}
+// ------------------------------------------------
+// ------------ Without connection pooling --------
+// ------------------------------------------------
+
+// function getConnection(){
+// 	var connection = mysql.createConnection({
+// 	  	host     : process.env.DROPBOX_HOST,
+// 	  	user     : process.env.DROPBOX_USER,
+// 	  	password : process.env.DROPBOX_PASSWORD,
+// 	  	database : 'dropbox'
+// 	});	
+// 	return connection;
+// }
+
+// exports.executequery = function(query, callback){
+// 	var connection = getConnection();
+
+// 	connection.connect();
+// 	console.log("SQL Query:: " + query);
+// 	connection.query(query, function (err, rows, fields) {
+// 	  	if (err) callback(err, null);
+// 	  	else callback(null, rows);
+// 	});
+
+// 	connection.end();
+// 	console.log("Connection Ended!");
+// }
+
+
+// ------------------------------------------------
+// ----------- With Connection Pooling-------------
+// ------------------------------------------------
+
+var connection_pool = mysql.createPool({
+  	host     : process.env.DROPBOX_HOST,
+  	user     : process.env.DROPBOX_USER,
+  	password : process.env.DROPBOX_PASSWORD,
+  	database : 'dropbox'
+});	
+
 
 exports.executequery = function(query, callback){
-	var connection = getConnection();
+	
 
-	connection.connect();
-	console.log("SQL Query:: " + query);
-	connection.query(query, function (err, rows, fields) {
-	  	if (err) callback(err, null);
-	  	else callback(null, rows);
+	connection_pool.getConnection(function(err, connection){
+		connection.query(query, function (err, rows, fields) {
+			connection.release();
+		  	if (err) callback(err, null);
+		  	else callback(null, rows);
+		});
 	});
+	console.log("SQL Query:: " + query);
 
-	connection.end();
 	console.log("Connection Ended!");
 }
